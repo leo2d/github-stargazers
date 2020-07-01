420947e09567d6539b30c1dc35569c0e098d21aa
Leonardo Dias <contato.leonardod@yahoo.com>
2020-06-30 23:46:45 -0300

    chore: add custom user agent

--- src/shared/request.js
+++ src/shared/request.js
@@ -3,9 +3,14 @@ const { default: Axios } = require('axios');
 const config = require('../config');
 
 const requestGitHubData = async (url, onComplete, onError, key = '') => {
-  const headers = { Authorization: `token ${config.github.apiAccessToken}` };
+  const requestConfig = {
+    headers: {
+      Authorization: `token ${config.github.apiAccessToken}`,
+      'User-Agent': 'github-stargazers',
+    },
+  };
 
-  return Axios.get(url, { headers })
+  return Axios.get(url, requestConfig)
     .then((response) => onComplete(response, key))
     .catch((error) => onError(error, key));
 };
