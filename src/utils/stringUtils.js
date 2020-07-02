7c152bef61df1500f7c6c293e8614c4fee0536de
Leonardo Dias <contato.leonardod@yahoo.com>
2020-07-02 20:14:25 -0300

    fix: add numbers only validation on string utils

--- src/utils/stringUtils.js
+++ src/utils/stringUtils.js
@@ -2,6 +2,7 @@ const stringIsValid = (str) =>
   str !== null &&
   str !== undefined &&
   typeof str === 'string' &&
-  str.trim() !== '';
+  str.trim() !== '' &&
+  !str.match(/^[0-9]*$/);
 
 module.exports = { stringIsValid };
