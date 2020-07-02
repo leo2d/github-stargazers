14aab2602893eebaf4ae3362e93b03d25a44593d
Leonardo Dias <contato.leonardod@yahoo.com>
2020-07-02 19:10:38 -0300

    refactor: remove guard from repositories service
    that guard makes more sense on github service

--- src/services/repositoriesService.js
+++ src/services/repositoriesService.js
@@ -1,9 +1,6 @@
-const stringUtils = require('../utils/stringUtils');
 const { searchRepositoriesByOrg } = require('./githubService');
 
 const getRepositoriesInfoByOrg = async (org) => {
-  if (!stringUtils.stringIsValid(org)) throw new Error('Invalid args...');
-
   const reposInfo = await searchRepositoriesByOrg(org);
 
   const sortedRepos = reposInfo.sort((a, b) => (a.stars > b.stars ? -1 : 1));
